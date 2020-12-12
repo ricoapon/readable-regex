e4976ecaeefa5c7c3173c51cdb2198c6da259905
Rico Apon <rico.apon@hotmail.com>
2020-12-12 14:22:32 +0100

    Add SpotBugs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Readable Regex [![Build Status](https://travis-ci.com/GreenT13/readable-regex.svg?branch=master)](https://travis-ci.com/GreenT13/readable-regex)
+
+# Local development
+All additional plugins to check the code base should run when calling the following gradle command:
+```
+gradle checks
+```
+This will trigger [SpotBugs](https://spotbugs.github.io/). If you want to run this individually (because this is faster), you can use:
+````
+gradle spotbugs
+````
+The HTML reports are located in a subdirectory in `build/reports`.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("com.github.spotbugs") version "4.5.0"
 }
 
 java {
@@ -13,10 +14,36 @@ repositories {
 }
 
 dependencies {
+    implementation("com.github.spotbugs:spotbugs-annotations:4.1.1")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 val test by tasks.getting(Test::class) {
     useJUnitPlatform()
+}
+
+// ================
+// SpotBugs
+// ================
+spotbugs {
+    // Display final report as HTML.
+    // Use different HTML template (stylesheet) that is prettier.
+    tasks.spotbugsMain {
+        reports.create("html") {
+            isEnabled = true
+            setStylesheet("fancy-hist.xsl")
+        }
+    }
+    tasks.spotbugsTest {
+        reports.create("html") {
+            isEnabled = true
+            setStylesheet("fancy-hist.xsl")
+        }
+    }
+}
+tasks.register("spotbugs") {
+    dependsOn(tasks.spotbugsMain)
+    dependsOn(tasks.spotbugsTest)
 }
