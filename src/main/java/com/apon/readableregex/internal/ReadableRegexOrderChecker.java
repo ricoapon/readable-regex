78192a9ef0f9e8c2e76c3e4891d186918e0fb9da
Rico Apon <rico.apon@hotmail.com>
2020-12-15 17:25:06 +0100

    Use star import for methods

--- src/main/java/com/apon/readableregex/internal/ReadableRegexOrderChecker.java
+++ src/main/java/com/apon/readableregex/internal/ReadableRegexOrderChecker.java
@@ -3,6 +3,8 @@ package com.apon.readableregex.internal;
 import com.apon.readableregex.ReadableRegex;
 import com.apon.readableregex.ReadableRegexPattern;
 
+import static com.apon.readableregex.internal.MethodOrderChecker.Method.*;
+
 /**
  * Subclass of {@link ReadableRegexBuilder} to check whether methods are called in the right order using an instance
  * of {@link MethodOrderChecker}.
@@ -24,43 +26,43 @@ public class ReadableRegexOrderChecker extends ReadableRegexBuilder {
     public ReadableRegex regexFromString(String regex) {
         // We are not actually sure that the regex is a standalone block. If we don't do this however, it is never possible
         // to add a quantifier after this block. I leave the user responsible for the outcome.
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.STANDALONE_BLOCK);
+        methodOrderChecker.checkCallingMethod(STANDALONE_BLOCK);
         return super.regexFromString(regex);
     }
 
     @Override
     public ReadableRegex add(ReadableRegex regexBuilder) {
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.STANDALONE_BLOCK);
+        methodOrderChecker.checkCallingMethod(STANDALONE_BLOCK);
         return super.add(regexBuilder);
     }
 
     @Override
     public ReadableRegex literal(String literalValue) {
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.STANDALONE_BLOCK);
+        methodOrderChecker.checkCallingMethod(STANDALONE_BLOCK);
         return super.literal(literalValue);
     }
 
     @Override
     public ReadableRegex digit() {
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.STANDALONE_BLOCK);
+        methodOrderChecker.checkCallingMethod(STANDALONE_BLOCK);
         return super.digit();
     }
 
     @Override
     public ReadableRegex whitespace() {
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.STANDALONE_BLOCK);
+        methodOrderChecker.checkCallingMethod(STANDALONE_BLOCK);
         return super.whitespace();
     }
 
     @Override
     public ReadableRegex oneOrMore() {
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.QUANTIFIER);
+        methodOrderChecker.checkCallingMethod(QUANTIFIER);
         return super.oneOrMore();
     }
 
     @Override
     public ReadableRegex optional() {
-        methodOrderChecker.checkCallingMethod(MethodOrderChecker.Method.QUANTIFIER);
+        methodOrderChecker.checkCallingMethod(QUANTIFIER);
         return super.optional();
     }
 
