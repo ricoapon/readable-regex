6c30f2cc48af2d73138d0a1a1f208822f07a462f
Rico Apon <rico.apon@hotmail.com>
2020-12-17 13:53:48 +0100

    Add enum with supported pattern flags

---
+++ src/main/java/com/apon/readableregex/PatternFlag.java
@@ -0,0 +1,28 @@
+package com.apon.readableregex;
+
+import java.util.regex.Pattern;
+
+/**
+ * Enum representing all the possible flags of {@link java.util.regex.Pattern} that are useful for this library.
+ */
+public enum PatternFlag {
+    /** See {@link Pattern#CASE_INSENSITIVE}. */
+    CASE_INSENSITIVE(Pattern.CASE_INSENSITIVE),
+
+    /** See {@link Pattern#MULTILINE}. */
+    MULTILINE(Pattern.MULTILINE),
+
+    /** See {@link Pattern#DOTALL}. */
+    DOT_ALL(Pattern.DOTALL);
+
+    /** The integer that is used for setting the flag on {@link java.util.regex.Pattern}. */
+    private final int jdkPatternFlagCode;
+
+    PatternFlag(int jdkPatternFlagCode) {
+        this.jdkPatternFlagCode = jdkPatternFlagCode;
+    }
+
+    public int getJdkPatternFlagCode() {
+        return jdkPatternFlagCode;
+    }
+}
