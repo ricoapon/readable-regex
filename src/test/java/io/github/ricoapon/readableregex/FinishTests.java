62d319e0ddd30f8074be8eb452f323e557729567
Rico Apon <rico.apon@gmail.com>
2021-01-16 12:58:00 +0100

    Ignore SpotBugs error which seemed to  be a bug

--- src/test/java/io/github/ricoapon/readableregex/FinishTests.java
+++ src/test/java/io/github/ricoapon/readableregex/FinishTests.java
@@ -1,5 +1,6 @@
 package io.github.ricoapon.readableregex;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.junit.jupiter.api.Test;
 
 import static io.github.ricoapon.readableregex.ReadableRegex.regex;
@@ -10,6 +11,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 /**
  * Tests related to methods that are inside {@link FinishBuilder}.
  */
+@SuppressFBWarnings(value = "RV_RETURN_VALUE_IGNORED_NO_SIDE_EFFECT", justification = "Lambda inside throwNpeWhenTextToMatchIsNull " +
+        "triggers SpotBugs, but we know we don't use the return value. We expect to throw an NPE. " +
+        "For some reason, this only has to be ignored after commit f1a8d20a9611d1f940823e16721d6efaeb4d16ed. Before the " +
+        "(unrelated!) code was committed, this did not happen.")
 class FinishTests {
     private final static String REGEX = "a1?";
     private final ReadableRegex<?> readableRegex = regex(REGEX);
